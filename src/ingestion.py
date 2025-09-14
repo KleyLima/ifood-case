f6d68e7ac4cc72dc92eb9b26af9d955587b82b00
Kleyton Lima <kleytonlima@frstfalconi.com>
2025-09-14 20:07:49 -0300

    feat: get all files given a time range The download time may be a problem with a large range, maybe play along chunk_size make the trick

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,7 +1,7 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
-
-base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_2023-01.parquet"
+from datetime import datetime
+from itertools import product
 
 TAXI_TYPES = [
     "fhvhv", # High Volume For-Hire Vehicle Trip Records
@@ -10,7 +10,7 @@ TAXI_TYPES = [
     "fhv" # For-Hire Vehicle Trip Records 
 ]
 
-def download_file_stream(url, filename=None, chunk_size=30 * 1024):
+def download_file_stream(url, filename=None, chunk_size=15 * 1024 * 1024):
     response = requests.get(url, stream=True)
     response.raise_for_status()
     
@@ -23,7 +23,7 @@ def download_file_stream(url, filename=None, chunk_size=30 * 1024):
     
     total_size = int(response.headers.get('content-length', 0))
     
-    print(f"Downloading {filename}...")
+    print(f"Downloading {filename}...\n")
     if total_size:
         print(f"File size: {total_size / (1024*1024):.2f} MB")
     
@@ -41,7 +41,52 @@ def download_file_stream(url, filename=None, chunk_size=30 * 1024):
     print(f"\nDownload completed: {filename}")
     return filename
 
-if __name__ == "__main__":    
-    with ThreadPoolExecutor() as executor:
-        executor.map(download_file_stream, [base_url.format(taxi_type=t_type) for t_type in TAXI_TYPES])
+def get_all_date_combinations(start_date, end_date):
+    date_format = "%Y-%m"
+    start_date = datetime.strptime(start_date, date_format)
+    end_date = datetime.strptime(end_date, date_format)
     
+    combinations = []
+    current_year = start_date.year
+    current_month = start_date.month
+    
+    end_year = end_date.year
+    end_month = end_date.month
+    
+    while (current_year < end_year) or (current_year == end_year and current_month <= end_month):
+        combinations.append(f"{current_year}-{str(current_month).zfill(2)}")
+        
+        current_month += 1
+        if current_month > 12:
+            current_month = 1
+            current_year += 1
+    
+    return combinations
+
+def all_download_combinations(start_date, end_date):
+    """Create the base for all files available in TLC Trip Record Data (www.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
+    given two dates
+
+    Args:
+        start_date (str): Date in format "Year-Month"
+        end_date (str): Date in format "Year-Month"
+    
+    Returns: A list with all possible file names.
+    """
+    date_combinations = get_all_date_combinations(start_date, end_date)
+    combinations = [f"{taxi}_tripdata_{data}.parquet" for taxi, data in product(TAXI_TYPES, date_combinations)]    
+
+    return combinations
+
+def download_files(start_date, end_date):
+    file_names = all_download_combinations(start_date, end_date)
+    base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
+    
+    urls_to_download = [base_url + file_name for file_name in file_names]
+    with ThreadPoolExecutor() as executor:
+        executor.map(download_file_stream, urls_to_download)
+
+if __name__ == "__main__":    
+    # print(get_all_date_combinations("2023-01", "2025-05"))
+    # print(all_download_combinations("2023-01", "2023-05"))
+    download_files("2023-01", "2023-05")
