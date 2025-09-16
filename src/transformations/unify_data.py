4fb8c333a3acc9952ae5d1e08fcad86defdc27f0
Kleyton Lima <kley158@gmail.com>
2025-09-16 15:00:47 -0300

    feat: unify taxi sanitizated data to just one table on trusted layer

---
+++ src/transformations/unify_data.py
@@ -0,0 +1,18 @@
+from pyspark.sql.functions import lit
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("app").getOrCreate()
+
+
+green_data = spark.table("trusted.green")
+green_data_label = green_data.withColumn("cab_type", lit("green"))
+green_data_renamed = green_data_label.withColumnRenamed("lpep_pickup_datetime", "pickup_datetime").withColumnRenamed("lpep_dropoff_datetime", "dropoff_datetime")
+
+
+yellow_data = spark.table("trusted.yellow")
+yellow_data_label = yellow_data.withColumn("cab_type", lit("yellow"))
+yellow_data_renamed = yellow_data_label.withColumnRenamed("tpep_pickup_datetime", "pickup_datetime").withColumnRenamed("tpep_dropoff_datetime", "dropoff_datetime")
+
+taxi_data_unified = yellow_data_renamed.unionByName(green_data_renamed)
+
+taxi_data_unified.write.mode("overwrite").format("delta").saveAsTable("trusted.taxi_data_unified")
