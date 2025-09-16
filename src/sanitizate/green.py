2fdf75251a6e04b4e6bb6e25055a7bd851aad8c5
Kleyton Lima <kley158@gmail.com>
2025-09-16 14:29:46 -0300

    feat: sanitizate data to trusted layer

---
+++ src/sanitizate/green.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("app").getOrCreate()
+
+green_df = spark.table("raw.green") \
+    .select("VendorID", "passenger_count", "total_amount", 
+            "tpep_pickup_datetime", "tpep_dropoff_datetime")
+
+only_valid_vendors = green_df.filter(col("VendorID").isNotNull())
+
+valid_passenger_cnt = only_valid_vendors.filter(col("passenger_count") > 0)
+
+spark.sql("CREATE SCHEMA IF NOT EXISTS trusted")
+
+valid_passenger_cnt.write \
+        .mode("overwrite") \
+        .format("delta") \
+        .saveAsTable("trusted.green")
---
+++ src/sanitizate/yellow.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.appName("app").getOrCreate()
+
+yellow_df = spark.table("raw.yellow") \
+    .select("VendorID", "passenger_count", "total_amount", 
+            "tpep_pickup_datetime", "tpep_dropoff_datetime")
+
+only_valid_vendors = yellow_df.filter(col("VendorID").isNotNull())
+
+valid_passenger_cnt = only_valid_vendors.filter(col("passenger_count") > 0)
+
+spark.sql("CREATE SCHEMA IF NOT EXISTS trusted")
+
+valid_passenger_cnt.write \
+        .mode("overwrite") \
+        .format("delta") \
+        .saveAsTable("trusted.yellow")
