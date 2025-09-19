83393fc14f624cb0a7ba27b97b56ce7ba7e08d4c
Kleyton Lima <kley158@gmail.com>
2025-09-19 14:57:05 -0300

    feat: new sanitizations and logging

--- src/sanitizate/green.py
+++ src/sanitizate/green.py
@@ -3,19 +3,30 @@ from pyspark.sql.functions import col, year, month
 
 spark = SparkSession.builder.appName("app").getOrCreate()
 
-green_df = spark.table("raw.green") \
-    .select("VendorID", "passenger_count", "total_amount", 
-            "lpep_pickup_datetime", "lpep_dropoff_datetime")
+green_df = spark.table("raw.green").select(
+    "VendorID",
+    "passenger_count",
+    "total_amount",
+    "lpep_pickup_datetime",
+    "lpep_dropoff_datetime",
+)
 
-only_valid_vendors = green_df.filter(col("VendorID").isNotNull())
+only_valid_vendors = green_df.filter(
+    col("VendorID").isNotNull() & col("VendorID").isin([1, 2, 6])
+)
+print("Removed nullable and invalid vendor IDS")
+
 
 valid_passenger_cnt = only_valid_vendors.filter(col("passenger_count") > 0)
+print("Removing invalid passenger count")
 
-desired_range_only = valid_passenger_cnt.filter((year("lpep_pickup_datetime") == 2023) & (month("lpep_pickup_datetime") < 6))
+desired_range_only = valid_passenger_cnt.filter(
+    (year("lpep_pickup_datetime") == 2023) & (month("lpep_pickup_datetime") < 6)
+)
+print("Keeping data on desired range (January to May of 2023)")
 
 spark.sql("CREATE SCHEMA IF NOT EXISTS trusted")
+print("Schema created")
 
-valid_passenger_cnt.write \
-        .mode("overwrite") \
-        .format("delta") \
-        .saveAsTable("trusted.green")
+valid_passenger_cnt.write.mode("overwrite").format("delta").saveAsTable("trusted.green")
+print("Table created")
--- src/sanitizate/yellow.py
+++ src/sanitizate/yellow.py
@@ -3,19 +3,31 @@ from pyspark.sql.functions import col, year, month
 
 spark = SparkSession.builder.appName("app").getOrCreate()
 
-yellow_df = spark.table("raw.yellow") \
-    .select("VendorID", "passenger_count", "total_amount", 
-            "tpep_pickup_datetime", "tpep_dropoff_datetime")
+yellow_df = spark.table("raw.yellow").select(
+    "VendorID",
+    "passenger_count",
+    "total_amount",
+    "tpep_pickup_datetime",
+    "tpep_dropoff_datetime",
+)
 
-only_valid_vendors = yellow_df.filter(col("VendorID").isNotNull())
+only_valid_vendors = yellow_df.filter(
+    col("VendorID").isNotNull() & col("VendorID").isin([1, 2, 6, 7])
+)
+print("Removed nullable and invalid vendor IDS")
 
 valid_passenger_cnt = only_valid_vendors.filter(col("passenger_count") > 0)
+print("Removing invalid passenger count")
 
-desired_range_only = valid_passenger_cnt.filter((year("tpep_pickup_datetime") == 2023) & (month("tpep_pickup_datetime") < 6))
+desired_range_only = valid_passenger_cnt.filter(
+    (year("tpep_pickup_datetime") == 2023) & (month("tpep_pickup_datetime") < 6)
+)
+print("Keeping data on desired range (January to May of 2023)")
 
 spark.sql("CREATE SCHEMA IF NOT EXISTS trusted")
+print("Schema created")
 
-valid_passenger_cnt.write \
-        .mode("overwrite") \
-        .format("delta") \
-        .saveAsTable("trusted.yellow")
+valid_passenger_cnt.write.mode("overwrite").format("delta").saveAsTable(
+    "trusted.yellow"
+)
+print("Table created")
