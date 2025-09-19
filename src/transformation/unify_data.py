d60f6044a6afe978ef3970faade6016d15758ac0
Kleyton Lima <kley158@gmail.com>
2025-09-19 14:56:31 -0300

    chore: linting

--- src/transformation/unify_data.py
+++ src/transformation/unify_data.py
@@ -6,13 +6,19 @@ spark = SparkSession.builder.appName("app").getOrCreate()
 
 green_data = spark.table("trusted.green")
 green_data_label = green_data.withColumn("cab_type", lit("green"))
-green_data_renamed = green_data_label.withColumnRenamed("lpep_pickup_datetime", "pickup_datetime").withColumnRenamed("lpep_dropoff_datetime", "dropoff_datetime")
+green_data_renamed = green_data_label.withColumnRenamed(
+    "lpep_pickup_datetime", "pickup_datetime"
+).withColumnRenamed("lpep_dropoff_datetime", "dropoff_datetime")
 
 
 yellow_data = spark.table("trusted.yellow")
 yellow_data_label = yellow_data.withColumn("cab_type", lit("yellow"))
-yellow_data_renamed = yellow_data_label.withColumnRenamed("tpep_pickup_datetime", "pickup_datetime").withColumnRenamed("tpep_dropoff_datetime", "dropoff_datetime")
+yellow_data_renamed = yellow_data_label.withColumnRenamed(
+    "tpep_pickup_datetime", "pickup_datetime"
+).withColumnRenamed("tpep_dropoff_datetime", "dropoff_datetime")
 
 taxi_data_unified = yellow_data_renamed.unionByName(green_data_renamed)
 
-taxi_data_unified.write.mode("overwrite").format("delta").saveAsTable("trusted.taxi_data_unified")
+taxi_data_unified.write.mode("overwrite").format("delta").saveAsTable(
+    "trusted.taxi_data_unified"
+)
