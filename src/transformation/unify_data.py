641588ac88769d08972f726e49505bfbff4b09cd
Kleyton Lima <kley158@gmail.com>
2025-09-21 23:55:18 -0300

    fix: typo

--- src/transformation/unify_data.py
+++ src/transformation/unify_data.py
@@ -19,7 +19,7 @@ yellow_data_renamed = yellow_data_label.withColumnRenamed(
 
 taxi_data_unified = yellow_data_renamed.unionByName(green_data_renamed)
 
-taxi_data_unified = taxi_data_unified.withColumn("moth", month("pickup_datetime"))
+taxi_data_unified = taxi_data_unified.withColumn("month", month("pickup_datetime"))
 
 taxi_data_unified.write.mode("overwrite").format("delta").saveAsTable(
     "trusted.taxi_data_unified"
